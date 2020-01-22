d4beb63b9a8aaa439baaf810ccbe1be279306064
Nick Reardon <32754140+ThingOfNightmare@users.noreply.github.com>
2020-01-22 15:34:04 -0800

    Add project files.

---
+++ CS1D L3 PrefixAverages/main.cpp
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * AUTHOR 	  		: Nick Reardon
+ * Lab #3       	: Prefix Averages
+ * CLASS			: CS1D
+ * SECTION			: MW - 2:30p
+ * DUE DATE			: 01 / 27 / 20
+ *****************************************************************************/
+#include "main.h"
+
+using std::cout; using std::endl;
+
+
+int main()
+{
+
+	/*
+	 * HEADER OUTPUT
+	 */
+	PrintHeader(cout, "Prompt.txt");
+
+	/********************************************************************/
+
+
+
+	system("pause");
+	return 0;
+}
---
+++ CS1D L3 PrefixAverages/main.h
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ * AUTHOR 	  		: Nick Reardon
+ * Lab #3       	: Prefix Averages
+ * CLASS			: CS1D
+ * SECTION			: MW - 2:30p
+ * DUE DATE			: 01 / 27 / 20
+ *****************************************************************************/
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+//Standard includes
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "PrintHeader.h"
+
+//Program Specific
+
+
+#endif // _HEADER_H_
+
